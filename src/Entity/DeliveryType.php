e26423797d08ed97e0d7a9d8d6812ce8910110fe
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-10-19 23:39:39 +0200

    [Admin][DeliveryType] Update model with add delivery type model & implement this on admin to manage deliveries type

---
+++ src/Entity/DeliveryType.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of vins
+ *
+ * (c) Aurelien Morvan <morvan.aurelien@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Entity;
+
+use App\Entity\Traits\NameTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class DeliveryType
+ *
+ * @ORM\Table(name="amo_delivery_type")
+ * @ORM\Entity()
+ */
+class DeliveryType extends AbstractEntity
+{
+    use NameTrait;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(type="float")
+     */
+    protected $price;
+
+    /**
+     * @return float|null
+     */
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float|null $price
+     */
+    public function setPrice(?float $price): void
+    {
+        $this->price = $price;
+    }
+}
