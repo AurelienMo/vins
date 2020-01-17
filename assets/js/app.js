0e64d16ab4d869c33ffeea42c2287bc41cbe63da
Aurelien Morvan <morvan.aurelien@gmail.com>
2020-01-17 14:42:40 +0100

    [Front][Layout] Implement header || WIP

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
-import $ from 'jquery';
-import 'bootstrap';
+import '../scss/theme.scss'
 import '../scss/app.scss'
+var $ = require('jquery');
+global.$ = global.jQuery = $;
+import popper from 'popper.js';
+global.popper = global.Popper = popper;
+import 'mdbootstrap/js/bootstrap.min.js';
+
+//import 'mdbootstrap-pro/css/mdb.min.css';
+
+//global.bsCustomFileInput = require('mdbootstrap/js/modules/vendor/bs-custom-file-input');
+import 'mdbootstrap/js/mdb.min.js';
 import Modal from "./components/Modal";
 
 
