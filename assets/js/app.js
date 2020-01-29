92321e114dec5723a13e9fceb413a7e4233db5bd
Aurelien Morvan <morvan.aurelien@gmail.com>
2020-01-30 00:54:40 +0100

    Add Mdb Pro

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,7 @@ import '../scss/theme.scss'
 import '../scss/app.scss'
 var $ = require('jquery');
 global.$ = global.jQuery = $;
-import popper from 'popper.js';
-global.popper = global.Popper = popper;
-import 'mdbootstrap/js/bootstrap.min.js';
-//import 'mdbootstrap-pro/css/mdb.min.css';
 
 global.bsCustomFileInput = require('mdbootstrap/js/modules/bs-custom-file-input');
 import Global from "./components/Global";
 new Global();
-import 'mdbootstrap/js/mdb.min.js';
