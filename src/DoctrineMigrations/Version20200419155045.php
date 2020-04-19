5f61316ce3988e0dcdd2c187043e0f978a9ca3cb
Aurelien Morvan <aurelien.morvan@smile.fr>
2020-04-19 18:14:30 +0200

    Update image for detail wine & implement box admin

---
+++ src/DoctrineMigrations/Version20200419155045.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200419155045 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE amo_box_wine ADD short_phrase VARCHAR(255) NOT NULL, ADD image_path VARCHAR(255) DEFAULT NULL, ADD price DOUBLE PRECISION NOT NULL, ADD description LONGTEXT NOT NULL, ADD updated_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE amo_box_wine DROP short_phrase, DROP image_path, DROP price, DROP description, DROP updated_at');
+    }
+}
--- src/Entity/BoxWine.php
+++ src/Entity/BoxWine.php
@@ -14,18 +14,30 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Entity\Interfaces\OpinionElementInterface;
+use App\Entity\Interfaces\UpdatableInterface;
+use App\Entity\Traits\DescriptionTrait;
+use App\Entity\Traits\NameTrait;
+use App\Entity\Traits\TimeStampableTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Class BoxWine
  *
  * @ORM\Table(name="amo_box_wine")
  * @ORM\Entity(repositoryClass="App\Repository\BoxWineRepository")
+ *
+ * @Vich\Uploadable()
  */
-class BoxWine extends AbstractEntity implements OpinionElementInterface
+class BoxWine extends AbstractEntity implements OpinionElementInterface, UpdatableInterface
 {
+    use NameTrait;
+    use DescriptionTrait;
+    use TimeStampableTrait;
+
     /**
      * @var bool|null
      *
@@ -43,6 +55,33 @@ class BoxWine extends AbstractEntity implements OpinionElementInterface
      *     )
      */
     protected $wines;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $shortPhrase;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $imagePath;
+
+    /**
+     * @Vich\UploadableField(mapping="box_images", fileNameProperty="imagePath")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(type="float")
+     */
+    protected $price;
 
     public function __construct()
     {
@@ -79,5 +118,72 @@ class BoxWine extends AbstractEntity implements OpinionElementInterface
     public function removeCapaciy(Capacity $capacity)
     {
         $this->wines->removeElement($capacity);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getShortPhrase(): ?string
+    {
+        return $this->shortPhrase;
+    }
+
+    /**
+     * @param string|null $shortPhrase
+     */
+    public function setShortPhrase(?string $shortPhrase): void
+    {
+        $this->shortPhrase = $shortPhrase;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImagePath()
+    {
+        return $this->imagePath;
+    }
+
+    /**
+     * @param string $imagePath
+     */
+    public function setImagePath($imagePath): void
+    {
+        $this->imagePath = $imagePath;
+    }
+
+    /**
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param File $imageFile
+     */
+    public function setImageFile($imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+        if ($imageFile) {
+            $this->updateAt = new \DateTime('now');
+        }
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float|null $price
+     */
+    public function setPrice(?float $price): void
+    {
+        $this->price = $price;
     }
 }
